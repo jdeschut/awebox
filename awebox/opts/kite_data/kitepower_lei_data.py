e431b24175243580ac3a1928f7986486e0d1e363
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2025-01-09 12:43:31 +0100

    add example file and data file

---
+++ awebox/opts/kite_data/kitepower_lei_data.py
@@ -0,0 +1,77 @@
+#
+#    This file is part of awebox.
+#
+#    awebox -- A modeling and optimization framework for multi-kite AWE systems.
+#    Copyright (C) 2017-2020 Jochem De Schutter, Rachel Leuthold, Moritz Diehl,
+#                            ALU Freiburg.
+#    Copyright (C) 2018-2020 Thilo Bronnenmeyer, Kiteswarms Ltd.
+#    Copyright (C) 2016      Elena Malz, Sebastien Gros, Chalmers UT.
+#
+#    awebox is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 3 of the License, or (at your option) any later version.
+#
+#    awebox is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with awebox; if not, write to the Free Software Foundation,
+#    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#
+import numpy as np
+from casadi.tools import vertcat
+
+def data_dict():
+
+    data_dict = {}
+    data_dict['name'] = 'kitepower_lei'
+
+    data_dict['geometry'] = geometry() # kite geometry
+
+    stab_derivs, aero_validity = aero()
+    data_dict['stab_derivs'] = stab_derivs # stability derivatives
+    data_dict['aero_validity'] = aero_validity
+
+    return data_dict
+
+def geometry():
+
+    geometry = {}
+    geometry['b_ref'] = 10. # fill in some meaningful value
+    geometry['s_ref'] = 3.  # [m^2]
+    geometry['c_ref'] = geometry['s_ref'] / geometry['b_ref']  # [m]
+    
+    # TODO: add all relevant parameters here
+    geometry['ar'] = 10.0 # can be deleted later
+ 
+    # kite mass + KCU ?
+    geometry['m_k'] = 36.8  # [kg]
+
+    # tether attachment point
+    geometry['r_tether'] = np.zeros((3,1))
+
+    return geometry
+
+
+def aero():
+    # commented values are not currently supported, future implementation
+
+    # A reference model for airborne wind energy systems for optimization and control
+    # Article
+    # March 2019 Renewable Energy
+    # Elena Malz Jonas Koenemann S. Sieberling Sebastien Gros
+
+    # commented values are not currently supported, future implementation
+
+    stab_derivs = {}
+    aero_validity = {}
+
+    aero_validity['alpha_max_deg'] = 25.0
+    aero_validity['alpha_min_deg'] = -20.0
+    aero_validity['beta_max_deg'] = 20.0
+    aero_validity['beta_min_deg'] = -20.0
+    return stab_derivs, aero_validity
---
+++ examples/kitepower_lei_trajectory.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+"""
+Circular pumping trajectory for the Ampyx AP2 aircraft.
+Model and constraints as in:
+
+"Performance assessment of a rigid wing Airborne Wind Energy pumping system",
+G. Licitra, J. Koenemann, A. Bürger, P. Williams, R. Ruiterkamp, M. Diehl
+Energy, Vol.173, pp. 569-585, 2019.
+
+:author: Jochem De Schutter
+:edited: Rachel Leuthold
+"""
+
+import awebox as awe
+import awebox.opts.kite_data.ampyx_ap2_settings as ampyx_ap2_settings
+import matplotlib.pyplot as plt
+import numpy as np
+import awebox.tools.print_operations as print_op
+import awebox.opts.kite_data.kitepower_lei_data as kitepower_lei_data
+
+# indicate desired system architecture
+# here: single kite with 6DOF Ampyx AP2 model
+options = {}
+options['user_options.system_model.architecture'] = {1: 0}
+options['user_options.kite_standard'] = kitepower_lei_data.data_dict()
+options['user_options.system_model.wing_type'] = 'LEI'
+options['user_options.system_model.kite_dof'] = 3
+
+# indicate desired operation mode
+options['user_options.trajectory.type'] = 'power_cycle'
+options['user_options.trajectory.system_type'] = 'lift_mode'
+options['user_options.trajectory.lift_mode.windings'] = 1
+
+# indicate desired environment
+options['params.wind.z_ref'] = 100.0
+options['params.wind.power_wind.exp_ref'] = 0.15
+options['user_options.wind.model'] = 'power'
+options['user_options.wind.u_ref'] = 10.
+
+# indicate numerical nlp details
+# here: nlp discretization, with a zero-order-hold control parametrization, and
+# a simple phase-fixing routine. also, specify a linear solver to perform the Newton-steps
+# within ipopt.
+options['nlp.n_k'] = 40
+options['nlp.collocation.u_param'] = 'zoh'
+options['user_options.trajectory.lift_mode.phase_fix'] = 'simple' # 'single_reelout'
+options['solver.linear_solver'] = 'ma57'  # if HSL is installed, otherwise 'mumps'
+options['model.model_bounds.acceleration.include']  = False
+# (experimental) set to "True" to significantly (factor 5 to 10) decrease construction time
+# note: this may result in slightly slower solution timings
+options['nlp.compile_subfunctions'] = True
+
+
+# build and optimize the NLP (trial)
+trial = awe.Trial(options, 'Kitepower_LEI')
+trial.build()
+trial.optimize()
+
+# write the solution to CSV file, interpolating the collocation solution with given frequency.
+# trial.write_to_csv(filename = 'Ampyx_AP2_solution', frequency = 30)
+
+# draw some of the pre-coded plots for analysis
+trial.plot(['states', 'controls', 'constraints', 'quad'])
+
+# extract information from the solution for independent plotting or post-processing
+# here: plot relevant system outputs, compare to [Licitra2019, Fig 11].
+plot_dict = trial.visualization.plot_dict
+outputs = plot_dict['outputs']
+time = plot_dict['time_grids']['ip']
+avg_power = plot_dict['power_and_performance']['avg_power']/1e3
+
+print('======================================')
+print('Average power: {} kW'.format(avg_power))
+print('======================================')
+
+plt.subplots(5, 1, sharex=True)
+plt.subplot(511)
+plt.plot(time, plot_dict['x']['l_t'][0], label='Tether Length')
+plt.ylabel('[m]')
+plt.legend()
+plt.grid(True)
+
+plt.subplot(512)
+plt.plot(time, plot_dict['x']['dl_t'][0], label='Tether Reel-out Speed')
+plt.ylabel('[m/s]')
+plt.legend()
+plt.hlines([20, -15], time[0], time[-1], linestyle='--', color='black')
+plt.grid(True)
+
+plt.subplot(513)
+plt.plot(time, outputs['aerodynamics']['airspeed1'][0], label='Airspeed')
+plt.ylabel('[m/s]')
+plt.legend()
+plt.hlines([10, 32], time[0], time[-1], linestyle='--', color='black')
+plt.grid(True)
+
+plt.subplot(514)
+plt.plot(time, 180.0 / np.pi * outputs['aerodynamics']['alpha1'][0], label='Angle of Attack')
+plt.ylabel('[deg]')
+plt.legend()
+plt.hlines([9, -6], time[0], time[-1], linestyle='--', color='black')
+plt.grid(True)
+
+plt.subplot(515)
+plt.plot(time, outputs['local_performance']['tether_force10'][0], label='Tether Force Magnitude')
+plt.ylabel('[N]')
+plt.xlabel('t [s]')
+plt.legend()
+plt.hlines([50, 1800], time[0], time[-1], linestyle='--', color='black')
+plt.grid(True)
+
+plt.show()
