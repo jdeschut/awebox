2d3042bb46640ee49682fb4c0f2eb20e545c81b3
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2025-01-17 15:01:41 +0100

    update example file to obtain first solution

--- examples/kitepower_lei_trajectory.py
+++ examples/kitepower_lei_trajectory.py
@@ -29,7 +29,8 @@ options['user_options.system_model.kite_dof'] = 3
 # indicate desired operation mode
 options['user_options.trajectory.type'] = 'power_cycle'
 options['user_options.trajectory.system_type'] = 'lift_mode'
-options['user_options.trajectory.lift_mode.windings'] = 1
+windings = 3
+options['user_options.trajectory.lift_mode.windings'] = windings
 
 # indicate desired environment
 options['params.wind.z_ref'] = 100.0
@@ -40,25 +41,41 @@ options['user_options.wind.u_ref'] = 10.
 # coefficient boundaries
 options['model.system_bounds.x.coeff'] =  [np.array([-1., 0.]), np.array([1., 1.])]
 
-
 # indicate numerical nlp details
 # here: nlp discretization, with a zero-order-hold control parametrization, and
 # a simple phase-fixing routine. also, specify a linear solver to perform the Newton-steps
 # within ipopt.
-options['nlp.n_k'] = 40
+options['nlp.n_k'] = int(40/3 * windings)
 options['nlp.collocation.u_param'] = 'zoh'
 options['user_options.trajectory.lift_mode.phase_fix'] = 'simple' # 'single_reelout'
-options['solver.linear_solver'] = 'mumps'  # if HSL is installed, otherwise 'mumps'
+options['solver.linear_solver'] = 'ma57'  # if HSL is installed, otherwise 'mumps'
+options['model.system_bounds.x.ddl_t'] = [-2.0, 2.0]
+options['model.system_bounds.theta.t_f'] = [0.0, windings*20.0]
+
 options['model.model_bounds.acceleration.include']  = False
+options['model.model_bounds.aero_validity.include']  = False
+options['model.model_bounds.tether_stress.include']  = False
 # (experimental) set to "True" to significantly (factor 5 to 10) decrease construction time
 # note: this may result in slightly slower solution timings
-options['nlp.compile_subfunctions'] = False
+options['nlp.compile_subfunctions'] = True
 
+
+# initialization
+options['solver.initialization.shape'] = 'lemniscate'
+options['solver.initialization.lemniscate.az_width'] = 20*np.pi/180.
+options['solver.initialization.lemniscate.el_width'] = 8*np.pi/180.
+options['solver.initialization.inclination_deg'] = 30.
+options['solver.initialization.groundspeed'] = 20.
+options['solver.initialization.theta.diam_t'] = 5e-3
+options['solver.initialization.l_t'] = 300.0
+options['solver.max_iter_hippo'] = 1000
+options['solver.max_iter'] = 1000
+options['visualization.cosmetics.plot_ref'] = False
 
 # build and optimize the NLP (trial)
 trial = awe.Trial(options, 'Kitepower_LEI')
 trial.build()
-trial.optimize(final_homotopy_step = 'initial_guess')
+trial.optimize(final_homotopy_step = 'final')
 
 
 
@@ -66,7 +83,7 @@ trial.optimize(final_homotopy_step = 'initial_guess')
 # trial.write_to_csv(filename = 'Ampyx_AP2_solution', frequency = 30)
 
 # draw some of the pre-coded plots for analysis
-trial.plot(['states', 'controls', 'constraints', 'quad'])
+trial.plot(['isometric', 'states', 'controls', 'constraints'])
 
 # extract information from the solution for independent plotting or post-processing
 # here: plot relevant system outputs, compare to [Licitra2019, Fig 11].
@@ -80,39 +97,46 @@ print('======================================')
 print('Average power: {} kW'.format(avg_power))
 print('======================================')
 
-plt.subplots(5, 1, sharex=True)
-plt.subplot(511)
+plt.subplots(6, 1, sharex=True)
+plt.subplot(611)
 plt.plot(time, plot_dict['x']['l_t'][0], label='Tether Length')
 plt.ylabel('[m]')
 plt.legend()
 plt.grid(True)
 
-plt.subplot(512)
+plt.subplot(612)
 plt.plot(time, plot_dict['x']['dl_t'][0], label='Tether Reel-out Speed')
 plt.ylabel('[m/s]')
 plt.legend()
 plt.hlines([20, -15], time[0], time[-1], linestyle='--', color='black')
 plt.grid(True)
 
-plt.subplot(513)
+plt.subplot(613)
 plt.plot(time, outputs['aerodynamics']['airspeed1'][0], label='Airspeed')
 plt.ylabel('[m/s]')
 plt.legend()
 plt.grid(True)
 
-plt.subplot(514)
+plt.subplot(614)
 plt.plot(time, outputs['aerodynamics']['alpha1'][0], label='Angle of Attack')
 plt.ylabel('[deg]')
 plt.legend()
-plt.hlines([9, -6], time[0], time[-1], linestyle='--', color='black')
+plt.hlines([20, -20], time[0], time[-1], linestyle='--', color='black')
 plt.grid(True)
 
-plt.subplot(515)
+plt.subplot(615)
 plt.plot(time, outputs['local_performance']['tether_force10'][0], label='Tether Force Magnitude')
 plt.ylabel('[N]')
 plt.xlabel('t [s]')
 plt.legend()
-plt.hlines([50, 1800], time[0], time[-1], linestyle='--', color='black')
 plt.grid(True)
+
+plt.subplot(616)
+plt.plot(time, outputs['aerodynamics']['f_aero_earth1'][0], label='Aero Force X')
+plt.ylabel('[N]')
+plt.xlabel('t [s]')
+plt.legend()
+plt.grid(True)
+
 
 plt.show()
