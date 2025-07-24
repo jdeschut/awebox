845c21612af540c7ecdf37c4813d883c863cdea2
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2025-07-24 16:16:37 +0200

    fix cherry pick

--- awebox/mdl/dynamics.py
+++ awebox/mdl/dynamics.py
@@ -327,11 +327,7 @@ def make_output_structure(outputs, system_variables, parameters):
 
     # generate "empty" structure
     out_struct = cas.struct_symMX(full_list)
-<<<<<<< HEAD
-    # generate structure with SX expressions
-=======
     # generate structure with MX expressions
->>>>>>> 2aea200... change model construction to MX types for compilation purposes
     outputs_struct = out_struct(outputs_vec)
     # generate outputs function
     outputs_fun = cas.Function('outputs', [system_variables['scaled'], parameters], [outputs_struct.cat])
